a354bbd1eba4154cca70c497088172e045b117fb
Brett Moran <bmoran.bam@gmail.com>
2025-11-21 10:40:59 -0500

    Fix tab switch, add links back to game mechanics section

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // Function for switching tabs between SDT needs within game mechanics sections
-function gmsdtTabSwitch(event, gmNameClass, gmTabId) {
+function gmsdtTabSwitch(event, gmTabClass, gmContentClass, gmContentID) {
     // Code borrowed and modified from:
     // https://www.w3schools.com/howto/howto_js_tabs.asp
 
-    // gmNameClass is the class for this gm-<game_mechanic>
-    // gmTabId is the id for the gm-<game_mechanic>-<sdt_need> tab
+    // gmContentClass is the class for this gm-<game_mechanic>
+    // gmContentID is the id for the gm-<game_mechanic>-<sdt_need> tab
     var i, gmTabContent, gmTabSwitches;
     const activeClassName = "active";
 
     // Grab all of the gm-tab options
-    gmTabContent = document.getElementsByClassName(gmNameClass);
+    gmTabContent = document.getElementsByClassName(gmContentClass);
     for (i = 0; i < gmTabContent.length; i++) {
         gmTabContent[i].style.display = "none";
     }
 
-    gmTabSwitches = document.getElementsByClassName("gm-tab-option");
+    gmTabSwitches = document.getElementsByClassName(gmTabClass);
     for (i = 0; i < gmTabSwitches.length; i++) {
         gmTabSwitches[i].classList.remove(activeClassName);
     }
-    document.getElementById(gmTabId).style.display = "block";
+    document.getElementById(gmContentID).style.display = "block";
     event.currentTarget.classList.add(activeClassName);
 }
